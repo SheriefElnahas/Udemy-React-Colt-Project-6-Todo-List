ebb6d3bbf9a2cafcf576fe9276f241d8ebdf5ec0
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-30 06:05:38 +0200

    change App from functional to class component

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
 import './App.css';
 
+import React from 'react';
+
 import Header from "./components/Header";
 import Todo from './components/Todo';
 import NewTodoForm from './components/NewTodoForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      < Todo />
-      < Todo />
-      <NewTodoForm />
-    </div>
-  )
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {todos: [
+      {
+        todoTitle: 'Item 1',
+        todoId : 0
+      }
+    ]}
+  }
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        < Todo />
+        < Todo />
+        <NewTodoForm />
+      </div>
+    )
+  }
+
 }
 
 export default App
