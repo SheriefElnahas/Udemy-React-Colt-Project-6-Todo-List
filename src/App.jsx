3492792071c72638599f3236ee02c3102f7324fb
Sherief Elnahas <sheriefelnahas5@gmail.com>
2022-09-30 05:46:14 +0200

    Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-function App() {
- 
+import './App.css';
+import Header from "./components/Header";
 
+function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
     </div>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './Header.css';
+
+function Header() {
+    return <header className="Header">
+        <h1>Todo List!</h1>
+        <p>A Simple React Todo List App</p>
+    </header>
+}
+
+export default Header;
